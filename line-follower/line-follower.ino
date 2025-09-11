b205b2eff56639f72a8af64abce766d065b3012e
Juan Granados <juanlgu27@gmail.com>
2025-09-10 23:44:11 -0600

    probando el seguidor de lineas, parece funcionar menos el S1

--- line-follower/line-follower.ino
+++ line-follower/line-follower.ino
@@ -1,10 +1,18 @@
-
+// PINES DE PUENTE H
 const int AIA = 8;   // (pwm) pin 6 conectado a pin A-IA
 const int AIB = 7;   // (pwm) pin 5 conectado a pin A-IB
 const int BIA = 10;  // (pwm) pin 10 conectado a pin B-IA --> velodicdad
 const int BIB = 9;   // (pwm) pin 9 conectado a pin B-IB  --> direccion
-
 byte SPEED = 200;
+
+// PINES DE MODULO DE SEGUIDOR DE LINEA
+const int S1 = A7;
+const int S2 = A5;
+const int S3 = A4;
+const int S4 = A3;
+const int S5 = A2;
+const int CLP = A1;
+const int NEAR = A0;
 
 void setup() {
   hBridge();
@@ -12,18 +20,28 @@ void setup() {
 }
 
 void loop() {
-  forward();
-  delay(5000);
-  softStop();
+  // forward();
+  // delay(5000);
+  // softStop();
+  // delay(1000);
+  // backward();
+  // delay(5000);
+  // hardStop();
+  // delay(1000);
+  // left();
+  // delay(5000);
+  // right();
+  // delay(5000);
+
+  Serial.print("S1: " + String(digitalRead(S1)));
+  Serial.print(", S2: " + String(digitalRead(S2)));
+  Serial.print(", S3: " + String(digitalRead(S3)));
+  Serial.print(", S4: " + String(digitalRead(S4)));
+  Serial.print(", S5: " + String(digitalRead(S5)));
+  Serial.print(", CLP: " + String(digitalRead(CLP)));
+  Serial.print(", NEAR: " + String(digitalRead(NEAR)));
+  Serial.println();
   delay(1000);
-  backward();
-  delay(5000);
-  hardStop();
-  delay(1000);
-  left();
-  delay(5000);
-  right();
-  delay(5000);
 }
 
 
@@ -38,6 +56,16 @@ void hBridge() {
 void serial() {
   Serial.begin(9600);  // Inicia la comunicación a 9600 baudios
   Serial.println("Hola, consola serial!");
+}
+
+void lineFollower() {
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
+  pinMode(CLP, INPUT);
+  pinMode(NEAR, INPUT);
 }
 
 /* ================================  MOVEMENT ================================ */
--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ Processor: ATMega328P(Old bootloader)
 
 # Puente H: L9110
 https://github.com/Pumble/techfest-2023/blob/86c6783bc69c4bcfa216a150f764a18a7ffdc281/Pruebas/puente_h_v3/puente_h_v3.ino#L2
+
+# Five Channel Line Follower module
+https://www.crcibernetica.com/five-channel-line-follower-module/?searchid=2491466&search_query=Five+Channel+Line+Follower+module
+
+tutorial: https://arduinoinfo.mywikis.net/wiki/Robot%207-wayMultiTrackingSensor
+
+- Por alguna razon el S1 no parece funcionar, hay que probarlo en digital a ver que
+- 
